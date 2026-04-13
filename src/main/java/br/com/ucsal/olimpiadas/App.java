6fd1d6942f17d5f3c4a233ca93fe5b6f771182b8
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-04-12 22:44:15 -0300

    Separação do loop da classe main, indo para a classe Init Loop

--- src/main/java/br/com/ucsal/olimpiadas/App.java
+++ src/main/java/br/com/ucsal/olimpiadas/App.java
@@ -8,56 +8,39 @@ import br.com.ucsal.olimpiadas.domain.repository.MemoryRepository.ParticipanteMe
 import br.com.ucsal.olimpiadas.domain.repository.MemoryRepository.ProvaMemoryRepository;
 import br.com.ucsal.olimpiadas.domain.repository.MemoryRepository.QuestaoMemoryRepository;
 import br.com.ucsal.olimpiadas.domain.repository.MemoryRepository.TentativaMemoryRepository;
-import br.com.ucsal.olimpiadas.initialization.Init;
+import br.com.ucsal.olimpiadas.initialization.InitLoop;
+import br.com.ucsal.olimpiadas.initialization.Seed;
 import br.com.ucsal.olimpiadas.initialization.ItemDeclaration;
 import br.com.ucsal.olimpiadas.menu.Menu;
 import br.com.ucsal.olimpiadas.menu.itensMenu.*;
 import br.com.ucsal.olimpiadas.service.PontuacaoService;
 import br.com.ucsal.olimpiadas.ui.UiConsole;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class App {
-    private final Scanner in = new Scanner(System.in);
-
-    private Menu menu = new Menu();
-
-
-    private IParticipanteRepository participanteRepository = new ParticipanteMemoryRepository();
-    private IProvaRepository provaRepository = new ProvaMemoryRepository();
-    private IQuestaoRepository questaoRepository = new QuestaoMemoryRepository();
-    private ITentativaRepository tentativaRepository = new TentativaMemoryRepository();
-
-    private PontuacaoService pontuacaoService = new PontuacaoService();
-    private UiConsole uiConsole = new UiConsole(participanteRepository, provaRepository, in);
-    private final Init init = new Init(provaRepository, questaoRepository);
-
-    private final Map<String, ItemMenu> itemMenu
-            = ItemDeclaration.decItem(in, participanteRepository, provaRepository, questaoRepository, uiConsole, tentativaRepository, pontuacaoService);
-
     public static void main(String[] args) {
-        App app = new App();
-        app.start();
+        final Scanner in = new Scanner(System.in);
+
+        Menu menu = new Menu();
+
+        IParticipanteRepository participanteRepository = new ParticipanteMemoryRepository();
+        IProvaRepository provaRepository = new ProvaMemoryRepository();
+        IQuestaoRepository questaoRepository = new QuestaoMemoryRepository();
+        ITentativaRepository tentativaRepository = new TentativaMemoryRepository();
+
+        PontuacaoService pontuacaoService = new PontuacaoService();
+        UiConsole uiConsole = new UiConsole(participanteRepository, provaRepository, in);
+        final Seed seed = new Seed(provaRepository, questaoRepository);
+        InitLoop initLoop = new InitLoop();
+
+        final Map<String, ItemMenu> itemMenu
+                = ItemDeclaration.decItem(in, participanteRepository, provaRepository, questaoRepository, uiConsole, tentativaRepository, pontuacaoService);
+
+        InitLoop.start(seed, menu, itemMenu, in);
+
     }
 
-    private void start() {
-        init.seed();
-        while (true) {
-            menu.mostraMenu(itemMenu);
-            String ch = in.nextLine();
-            if (ch.equals("0")) {
-                System.out.println("Tchau");
-                break;
-            }
-            ItemMenu opcoes = itemMenu.get(ch);
-            if (opcoes != null) {
-                opcoes.action();
-            } else {
-                System.out.println("Opção inválida");
-            }
-        }
-    }
 
 }
